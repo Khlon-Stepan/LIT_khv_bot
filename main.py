6a1051d83e49c107beb018f6999a4bbd531875aa
Степан Хлонь <gutark@MacBook-Air-Stepan.local>
2025-03-03 20:10:44 +1000

    sec commit

--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-from config import TOKEN
-
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InputFile
 import aiohttp
 import asyncio
 from datetime import datetime, timedelta
 import config
+from aiogram.types import FSInputFile
 
 
-TOKEN = config.BOT_TOKEN
+TOKEN = config.TOKEN
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+kb1 = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text="Расписание")],
+        [KeyboardButton(text="Новости"), 
+         KeyboardButton(text="Посвящение в лицеисты 1488")],
+    ])
+
 @dp.message(Command(commands=["start"]))
 async def cmd_start(message: Message):
-    await message.reply(
-        "Привет! Я - официальный бот Лицея Инновационных Технологий г. Хабаровска\n\nЧем я могу тебе помочь?"
+    await message.answer(
+        "Привет! Я оффициальный бот лицея инновационных технологий. \n Чем я могу Вам помочь?",
+        reply_markup=kb1
     )
+@dp.message(lambda message: message.text in ["Расписание", "Новости", "Посвящение в лицеисты 1488"])
+async def weather_buttons(message: Message):
+    await message.answer_photo(photo=FSInputFile('hent.jpg', filename='Снеговик'), caption='Это снеговик!')
 
-async def main():    
+
+async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
